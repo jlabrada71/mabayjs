d02d703d47c330db5cd6e3665223856f0e831bee
Juan Labrada <jlabrada@yahoo.com>
2020-07-14 21:17:23 -0300

    added code-writer. There should be one manifesto by technology

---
+++ output/policy.vue
@@ -0,0 +1,13 @@
+
+  <div id="form">
+  <!-- policy -->
+    <v-form v-model="valid">
+      <v-container>
+        <v-row>
+           <p><td>id</td><td>{{policy.id}}</td></p>
+           <p><td>age</td><td>{{policy.age}}</td></p>
+
+        </v-row>
+      </v-container>
+    </v-form>
+</div>
---
+++ sample1.sh
@@ -0,0 +1,1 @@
+node generate.js ideal_model02.yml ./templates ./templates/vue-vuetify/manifesto1.json ./output
