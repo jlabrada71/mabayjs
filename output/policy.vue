9aab6d8b7fafd4965dbc27ca2faa75d7c838f5da
Juan Labrada <jlabrada@yahoo.com>
2020-07-16 00:21:58 -0300

    composite implemented

--- output/policy.vue
+++ output/policy.vue
@@ -1,13 +1,32 @@
 
+
   <div id="form">
   <!-- policy -->
     <v-form v-model="valid">
       <v-container>
         <v-row>
            <p><td>id</td><td>{{policy.id}}</td></p>
-           <p><td>age</td><td>{{policy.age}}</td></p>
+<p><td>age</td><td>{{policy.age}}</td></p>
 
         </v-row>
       </v-container>
     </v-form>
 </div>
+
+  name: 'policy',
+  data: () => ({
+    valid: false,
+    : '',
+: '',
+
+  }),
+
+
+.id {
+
+}
+.age {
+
+}
+
+
