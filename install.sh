2c97e2ccb966910f51a31e4b0586a99cf2a312fb
Juan Labrada <jlabrada@yahoo.com>
2020-07-27 22:06:59 -0300

    install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+ cp output/routes/* ../heroku/juanlabrada/routes
+ cp output/lib/* ../heroku/juanlabrada/lib
+ cp output/src/components/* ../heroku/juanlabrada/src/components
+ cp output/src/lib/* ../heroku/juanlabrada/src/lib
+ cp output/src/router/* ../heroku/juanlabrada/src/router
+ cp output/src/views/* ../heroku/juanlabrada/src/views
