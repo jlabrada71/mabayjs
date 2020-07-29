6dc6a236c60e1291fbaee4eb8946848f19e4d812
Juan Labrada <jlabrada@yahoo.com>
2020-07-29 02:00:34 -0300

    Test for sending email

---
+++ tests/sendmail.test.js
@@ -0,0 +1,49 @@
+var nodemailer = require('nodemailer');
+
+
+describe('Send email tests', () => {
+  beforeEach(() => {
+    // Clear all instances and calls to constructor and all methods:
+    //ProblemRepository.mockClear();
+
+  });
+
+  it('should generate code for value objects', () => {
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'juan.labrada@gmail.com',
+        pass: 'Luis@))^'
+      }
+    });
+
+    var mailOptions = {
+      from: 'juan.labrada@gmail.com',
+      to: 'jlabrada@yahoo.com',
+      subject: 'Sending Email using Node.js',
+      text: 'That was easy!'
+    };
+
+    var mailOptionsMultipleRecivers = {
+      from: 'youremail@gmail.com',
+      to: 'myfriend@yahoo.com, myotherfriend@yahoo.com',
+      subject: 'Sending Email using Node.js',
+      text: 'That was easy!'
+    }
+
+    var mailOptionsHtml = {
+      from: 'youremail@gmail.com',
+      to: 'myfriend@yahoo.com',
+      subject: 'Sending Email using Node.js',
+      html: '<h1>Welcome</h1><p>That was easy!</p>'
+    }
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  });
+});
