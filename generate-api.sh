edd04fe820c9ff01b7d9c73bba9e0499eca76ddb
jlabrada <jlabrada@yahoo.com>
2022-08-24 17:48:29 -0300

    API generation simplified

---
+++ generate-api.sh
@@ -0,0 +1,4 @@
+node generate.js models/juanlabrada/$1.yml ./templates/create ./templates/create/nodejs/lib/manifesto.json ./output
+node generate.js models/juanlabrada/$1.yml ./templates/create ./templates/create/nodejs/route-reg/manifesto.json ./output
+node generate.js models/juanlabrada/$1.yml ./templates/create ./templates/create/nodejs/routes/manifesto.json ./output
+node generate.js models/juanlabrada/$1.yml ./templates/create ./templates/create/api-client/manifesto.json ./output
