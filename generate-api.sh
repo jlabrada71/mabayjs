f17c8795a6a68f4a4137d1b043758e36a364bc93
jlabrada <jlabrada@yahoo.com>
2023-01-11 11:19:15 -0300

    Improvig generate-api.sh interface

--- generate-api.sh
+++ generate-api.sh
@@ -1,9 +1,18 @@
-if ["$1" == ""] 
-then
+#!/bin/bash
+
+MODEL_DIRECTORY=$2
+if [ "$MODEL_DIRECTORY" == "" ]; then
+  MODEL_DIRECTORY=quick-start
+fi
+
+MODEL=$1.yml
+if [ "$MODEL" == ".yml" ]; then
   echo "usage: ./generate-api.sh <model>   (don't include .yml)"
 else
-  node generate.js models/juanlabrada/$1.yml ./templates/create ./templates/create/nodejs/lib/manifesto.json ./output
-  node generate.js models/juanlabrada/$1.yml ./templates/create ./templates/create/nodejs/route-reg/manifesto.json ./output
-  node generate.js models/juanlabrada/$1.yml ./templates/create ./templates/create/nodejs/routes/manifesto.json ./output
-  node generate.js models/juanlabrada/$1.yml ./templates/create ./templates/create/api-client/manifesto.json ./output
+  MODEL=$1.yml
+  echo "using model $MODEL_DIRECTORY/$MODEL"
+  node generate.js models/$MODEL_DIRECTORY/$MODEL ./templates/create ./templates/create/nodejs/lib/manifesto.json ./output
+  node generate.js models/$MODEL_DIRECTORY/$MODEL ./templates/create ./templates/create/nodejs/route-reg/manifesto.json ./output
+  node generate.js models/$MODEL_DIRECTORY/$MODEL ./templates/create ./templates/create/nodejs/routes/manifesto.json ./output
+  node generate.js models/$MODEL_DIRECTORY/$MODEL ./templates/create ./templates/create/api-client/manifesto.json ./output
 fi
