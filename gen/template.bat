c3590495c36cdf3ab1d040b437205cd153d4a0fd
Lino Aboy <linodeveloper85@gmail.com>
2023-01-12 22:49:04 -0500

    add gen folder bat commands

---
+++ gen/template.bat
@@ -0,0 +1,3 @@
+@echo off
+
+node "generate_template.js" "%CD%\..\heroku\juanlabrada\routes\routes-register.js" "customer" "%CD%\templates" "nodejs" "route-reg" "javascript" "%CD%\output"
---
+++ gen/vuetify.bat
@@ -0,0 +1,3 @@
+@echo off
+
+node "generate.js" "ideal_model03.yml" "%CD%\templates" "%CD%\templates\vue-vuetify\manifesto1.json" "%CD%\output"
